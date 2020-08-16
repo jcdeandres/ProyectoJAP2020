f575558b9e1300d44469fc0eeaadd13d93aa3001
jcdeandres <jcdeandres96@gmail.com>
2020-08-15 22:37:00 -0300

    Entrega 1 - login y JSON de products.html

--- js/products.js
+++ js/products.js
@@ -1,6 +1,38 @@
+function showProductsList(productsList){
+    let htmlContentToAppend = "";
+    for(let i = 0; i < productsList.length; i++){
+        let product = productsList[i];
+
+            htmlContentToAppend += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ product.name + ` - `+ product.currency + ` `+ product.cost + `</h4>
+                            <small class="text-muted">` + product.soldCount + ` artículos</small>
+                        </div>
+                        <p class="mb-1">` + product.description + `</p>
+                    </div>
+                </div>
+            </a>
+            `
+
+        document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
+    }
+};
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
 
+    //JSON que devuelve en "resultObj" los productos
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok") {
+            showProductsList(resultObj.data);
+        }
+    });
 });
